18186546bfed704e7f34bb54a84a06a2a924f4a2
tejas <jtejas10@gmail.com>
2018-12-16 14:10:48 -0500

    Hospital screen added

---
+++ InClassExercisesStarter/HospitalViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HospitalViewController.swift
+//  InClassExercisesStarter
+//
+//  Created by Tejas Jadhav on 2018-12-16.
+//  Copyright © 2018 room1. All rights reserved.
+//
+
+import UIKit
+
+class HospitalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
